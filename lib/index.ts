9bf4dd131822f93405585516d18ab254b8878b11
Mihail Mojsoski <mihail@mojsoski.mk>
2025-03-25 19:29:32 +0100

    Add IBaseModel to index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export type { IDatabaseService } from "./database-service";
 export { createDatabaseService } from "./database-service";
 export { ModelRepository } from "./base/repository";
+export { IBaseModel } from "./base";
